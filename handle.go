9f3f7f65d9f853ee995e26eb95fca479f07ca3c9
Alex Page <egapxela@gmail.com>
2024-05-31 16:27:21 -0400

    working req/resp!

--- handle.go
+++ handle.go
@@ -2,81 +2,32 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
-	"http2/frame"
 	"http2/session"
-	"io"
 	"net"
+	"os"
+	"strings"
 )
 
-var ClientPreface = []byte("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")
-var UnexpectedPreface = errors.New("unexpected preface")
-
-func ConsumePreface(rd io.Reader) error {
-	preface := make([]byte, 24)
-	n, err := rd.Read(preface)
+func Handler(headers map[string][]string, data []byte) (map[string][]string, []byte) {
+	path := strings.TrimLeft(headers[":path"][0], "/")
+	data, err := os.ReadFile(path)
+	hl := make(map[string][]string)
 	if err != nil {
-		return err
+		fmt.Println(err)
+		hl[":status"] = []string{"500"}
+		return hl, nil
 	}
-	if n != 24 {
-		return UnexpectedPreface
-	}
-	for i, b := range ClientPreface {
-		if b != preface[i] {
-			return UnexpectedPreface
-		}
-	}
-	return nil
+	hl[":status"] = []string{"200"}
+	hl["content-type"] = []string{"text/plain"}
+
+	return hl, data
 }
 
 func HandleConnection(conn net.Conn) error {
 	defer conn.Close()
-
-	buf := bufio.NewReader(conn)
-	outbuf := bufio.NewWriter(conn)
-	sess := session.NewSession(buf, outbuf)
-
-	fmt.Println("======================= NEW CONNECTION =======================")
-	if err := ConsumePreface(buf); err != nil {
-		return err
-	}
-	globalStream := sess.Stream(0)
-
-	// Send empty settings for our preface.
-	globalStream.SendFrame(frame.FrameSettings, 0, nil)
-	outbuf.Flush()
-
-	stgs, err := globalStream.ExpectFrameType(frame.FrameSettings)
-	if err != nil {
-		return err
-	}
-	data := make([]uint8, stgs.Length)
-	if _, err := io.ReadFull(buf, data); err != nil {
-		return err
-	}
-	sl := session.SettingsListFromFramePayload(data)
-	fmt.Println("---(INITIAL CLIENT SETTINGS)---")
-	for _, item := range sl.Settings {
-		fmt.Printf("%s = %d\n", item.Type, item.Value)
-	}
-	fmt.Println("")
-	globalStream.SendFrame(frame.FrameSettings, session.STGS_ACK, nil)
-	outbuf.Flush()
-
-	for {
-		fh := new(frame.FrameHeader)
-		if err := fh.Unmarshal(buf); err != nil {
-			return err
-		}
-		data := make([]uint8, fh.Length)
-		if _, err := io.ReadFull(buf, data); err != nil {
-			return err
-		}
-		if err := sess.Dispatch(fh, data); err != nil {
-			outbuf.Flush()
-			return err
-		}
-		outbuf.Flush()
-	}
+	// Buffer the incoming connection.
+	sess := session.NewSession(bufio.NewReader(conn), conn)
+	sess.Handle = Handler
+	return sess.Serve()
 }
--- session/session.go
+++ session/session.go
@@ -1,6 +1,7 @@
 package session
 
 import (
+	"bytes"
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
@@ -10,6 +11,9 @@ import (
 	"io"
 )
 
+var ClientPreface = []byte("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")
+var UnexpectedPreface = errors.New("unexpected preface")
+
 // A Session object represents an open connection
 // between this server and a client.
 type Session struct {
@@ -18,49 +22,232 @@ type Session struct {
 
 	lastStream frame.Sid
 
+	Streams map[frame.Sid]*Stream
+
 	LookupTable *hpack.HeaderLookupTable
+
+	Handle func(map[string][]string, []byte) (map[string][]string, []byte)
 }
 
 func NewSession(rd io.Reader, wr io.Writer) *Session {
 	var sess Session
 	sess.Incoming = rd
 	sess.Outgoing = wr
-
 	sess.lastStream = 0
-
+	sess.Streams = make(map[frame.Sid]*Stream)
 	sess.LookupTable = hpack.NewHeaderLookupTable()
 	return &sess
 }
 
+func (sess *Session) ConsumePreface() error {
+	preface := make([]byte, 24)
+	n, err := io.ReadFull(sess.Incoming, preface)
+	if err != nil {
+		return err
+	}
+	if n != 24 {
+		return UnexpectedPreface
+	}
+	for i, b := range ClientPreface {
+		if b != preface[i] {
+			return UnexpectedPreface
+		}
+	}
+	return nil
+}
+
+func (sess *Session) Serve() error {
+	if err := sess.ConsumePreface(); err != nil {
+		return err
+	}
+	globalStream := sess.Stream(0)
+	// Server must initiate communications by sending
+	// a settings frame with initial settings. Leave it
+	// empty for now until we implement actual setting
+	// enforcement
+	if err := globalStream.SendFrame(frame.FrameSettings, 0, nil); err != nil {
+		return err
+	}
+
+	// Client will also send their initial settings.
+	// receive and acknowledge the frame.
+	_, data, err := sess.ExpectFrame(frame.FrameSettings, 0)
+	if err != nil {
+		return err
+	}
+	sl := SettingsListFromFramePayload(data)
+	fmt.Println("---(INITIAL CLIENT SETTINGS)---")
+	for _, item := range sl.Settings {
+		fmt.Printf("%s = %d\n", item.Type, item.Value)
+	}
+	globalStream.SendFrame(frame.FrameSettings, STGS_ACK, nil)
+
+	for {
+		fh, data, err := sess.ReadFrame()
+		if err != nil {
+			return err
+		}
+		if err := sess.Dispatch(fh, data); err != nil {
+			return err
+		}
+	}
+}
+
+func (sess *Session) readHeader() (*frame.FrameHeader, error) {
+	fh := new(frame.FrameHeader)
+	err := fh.Unmarshal(sess.Incoming)
+	if err != nil {
+		return nil, err
+	}
+	return fh, nil
+}
+
+func (sess *Session) readPayload(n uint32) ([]uint8, error) {
+	data := make([]uint8, n)
+	_, err := io.ReadFull(sess.Incoming, data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// Read a frame from the incoming connection. Returns the
+// frame header object + the frame payload if nonzero. If
+// the frame doesn't have a payload, ReadFrame returns a nil
+// slice.
+func (sess *Session) ReadFrame() (*frame.FrameHeader, []uint8, error) {
+	fh, err := sess.readHeader()
+	if err != nil {
+		return nil, nil, err
+	}
+	if fh.Length == 0 {
+		return fh, nil, nil
+	}
+	data, err := sess.readPayload(fh.Length)
+	if err != nil {
+		return nil, nil, err
+	}
+	return fh, data, nil
+}
+
+func (sess *Session) ExpectFrame(typ frame.FrameType, sid frame.Sid) (*frame.FrameHeader, []uint8, error) {
+	fh, err := sess.readHeader()
+	if err != nil {
+		return nil, nil, err
+	}
+	if fh.Type != typ {
+		return nil, nil, fmt.Errorf("expected type %s, got %s", typ, fh.Type)
+	}
+	if fh.Sid != sid {
+		return nil, nil, fmt.Errorf("expected sid %s, got %s", sid, fh.Sid)
+	}
+	if fh.Length == 0 {
+		return fh, nil, nil
+	}
+	data, err := sess.readPayload(fh.Length)
+	if err != nil {
+		return nil, nil, err
+	}
+	return fh, data, nil
+}
+
 func (sess *Session) Stream(sid frame.Sid) *Stream {
-	return &Stream{Sid: sid, Session: sess}
+	if st, ok := sess.Streams[sid]; ok {
+		return st
+	}
+	st := NewStream(sid, sess)
+	sess.Streams[sid] = st
+	return st
 }
 
 // Dispatch a frame header and payload to the appropriate handler.
 func (sess *Session) Dispatch(fh *frame.FrameHeader, data []uint8) error {
-	fmt.Println(fh)
+	// Last stream interacted with for error-sending
 	sess.lastStream = fh.Sid
 	switch fh.Type {
 	case frame.FrameSettings:
-		sl := SettingsListFromFramePayload(data)
-		for _, item := range sl.Settings {
-			fmt.Printf("%s = %d\n", item.Type, item.Value)
+		if fh.Flag(0) {
+			return nil
 		}
+		// Must acknowledge new settings frame
 		sess.Stream(0).SendFrame(frame.FrameSettings, STGS_ACK, nil)
+
 	case frame.FrameHeaders:
 		if err := sess.HandleHeader(fh, data); err != nil {
 			return err
 		}
+
 	case frame.FrameGoaway:
 		gf := GoawayFrameFromPayload(data)
 		fmt.Println("Received a GOAWAY from client")
 		fmt.Println(gf.String())
 		sess.SendGoaway(sess.lastStream, StreamErrorNoError, "")
 		return errors.New("client goaway")
+
+	case frame.FrameData:
+		buf := bytes.NewReader(data)
+		dataSize := fh.Length
+
+		// Frame is padded. The first byte of the payload
+		// is the pad length.
+		if fh.Flag(3) {
+			c, err := buf.ReadByte()
+			if err != nil {
+				return err
+			}
+			dataSize -= uint32(c) + 1
+		}
+		newData := make([]uint8, dataSize)
+		_, err := buf.Read(newData)
+		if err != nil {
+			return err
+		}
+		st := sess.Stream(fh.Sid)
+		st.ExtendData(newData)
+
+		// Bit 0 is END_STREAM
+		if fh.Flag(0) {
+			st.SetLocalClosed()
+		}
+
 	default:
-		fmt.Println(hex.Dump(data))
+		fmt.Printf("Unknown frame\n%s\n%s\n", fh, hex.Dump(data))
 	}
-	fmt.Println("------------------------------------------------------------")
+	st := sess.Stream(fh.Sid)
+	if fh.Sid != 0 && st.IsRemoteClosed() {
+		retHeaders, retData := sess.Handle(st.headers, st.data)
+		hl := hpack.NewHeaderList(sess.LookupTable)
+		if retHeaders == nil {
+			hl.Put(":status", "201")
+		} else {
+			if _, ok := retHeaders[":status"]; !ok {
+				if retData != nil {
+					hl.Put(":status", "200")
+				} else {
+					hl.Put(":status", "201")
+				}
+			}
+			for k, vs := range retHeaders {
+				if vs == nil {
+					continue
+				}
+				for _, v := range vs {
+					hl.Put(k, v)
+				}
+			}
+		}
+		flg := FLAG_END_HEADERS
+		if retData == nil {
+			flg |= FLAG_END_STREAM
+		}
+		st.SendFrame(frame.FrameHeaders, flg, hl.Dump())
+		if retData == nil {
+			return nil
+		}
+		st.SendFrame(frame.FrameData, FLAG_END_STREAM, retData)
+		st.SetLocalClosed()
+		sess.SendGoaway(fh.Sid, StreamErrorNoError, "")
+	}
 	return nil
 }
 
@@ -84,6 +271,7 @@ func (sess *Session) HandleHeader(fh *frame.FrameHeader, data []uint8) error {
 	totRead := 0
 	padLength := 0
 
+	st := sess.Stream(fh.Sid)
 	// End of headers
 	if !fh.Flag(2) {
 		// TODO: This server doesn't support continuation frames yet. fix it!
@@ -112,15 +300,13 @@ func (sess *Session) HandleHeader(fh *frame.FrameHeader, data []uint8) error {
 		if err != nil {
 			return err
 		}
-		fmt.Printf("%s: %s\n", k, v)
+		st.AddHeader(k, v)
 		if hdr.ShouldIndex() {
 			sess.LookupTable.Insert(k, v)
 		}
 	}
-	// End of stream
 	if fh.Flag(0) {
-		fmt.Printf("END OF STREAM %d\n", fh.Sid)
+		st.SetRemoteClosed()
 	}
-	fmt.Println(sess.LookupTable)
 	return nil
 }
